93e2bd621d796c83f9a3df3155cc0ef6fbc1b62f
Ricardo <l42647@alunos.uevora.pt>
2022-01-16 20:26:08 +0000

    deploy: Sensor module

---
+++ RunningEvents-Sensor/src/main/java/com/mycompany/runningevents/sensor/sensorApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.runningevents.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sensorApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(sensorApplication.class, args);
+    }
+}
