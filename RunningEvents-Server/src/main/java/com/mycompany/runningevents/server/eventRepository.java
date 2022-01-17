5c6ecc9ae8ec9016a201211fcd42ef1ded422d33
Ricardo <l42647@alunos.uevora.pt>
2022-01-17 11:46:12 +0000

    clean: Unused imports

--- RunningEvents-Server/src/main/java/com/mycompany/runningevents/server/eventRepository.java
+++ RunningEvents-Server/src/main/java/com/mycompany/runningevents/server/eventRepository.java
@@ -4,7 +4,6 @@
  */
 package com.mycompany.runningevents.server;
 
-import java.sql.Timestamp;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- RunningEvents-Server/src/main/java/com/mycompany/runningevents/server/participantController.java
+++ RunningEvents-Server/src/main/java/com/mycompany/runningevents/server/participantController.java
@@ -4,7 +4,6 @@
  */
 package com.mycompany.runningevents.server;
 
-import java.util.List;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
